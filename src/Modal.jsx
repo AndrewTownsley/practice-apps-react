d419245a4d326860a88312d66250b937b499cbe3
Andrew TOwnsley <atownsley86@gmail.com>
2021-06-15 19:47:01 -0600

    working on modal styling

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -5,14 +5,20 @@ const Modal = ({ user, showModal, closeModal, userData, setUserData, results}) =
   return (
     <>
       <div className={showModal ? "show-modal" : "hide-modal"}>
-        <div className="modal-window">
-            <h3>Modal Component</h3>
+        <div className="modal-wrapper">
+          <div className="modal">
+              <h3>Contact information</h3>
             <img src={user.picture.medium} alt={user.name.first} />
-            <div className="modal-window-info">
-              <h3>{user.name.first + " " + user.name.last}</h3>
-              <p></p>
+            <div className="modal-info">
+              <h3>{user.name.title + " " + user.name.first + " " + user.name.last}</h3>
+              <p>phone: {user.cell}</p>
+              <a href="/#">Email: {user.email}</a>
+              <p>Country: {user.location.country}</p>
+              <p>City: {user.location.city}</p>
+              <p>State/Province: {user.location.state}</p>
             </div>
             <button onClick={closeModal}>Close</button>
+          </div>
         </div>
           </div>
     </>
