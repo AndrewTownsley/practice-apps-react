5a20ffbb0045c34a27131d607ab8d32de0cf27b0
Andrew TOwnsley <atownsley86@gmail.com>
2021-06-15 22:58:25 -0600

    modal-styling is done for now.  Now working on closing the modal by clicking outside of it.

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -7,17 +7,22 @@ const Modal = ({ user, showModal, closeModal, userData, setUserData, results}) =
       <div className={showModal ? "show-modal" : "hide-modal"}>
         <div className="modal-wrapper">
           <div className="modal">
+            <div className="modal-header">
               <h3>Contact information</h3>
+              <button onClick={closeModal}><i className="fas fa-times 2x"></i></button>
+            </div>
             <img src={user.picture.medium} alt={user.name.first} />
             <div className="modal-info">
-              <h3>{user.name.title + " " + user.name.first + " " + user.name.last}</h3>
-              <p>phone: {user.cell}</p>
-              <a href="/#">Email: {user.email}</a>
+              <div className="modal-info-title">
+                <h3>{user.name.title + " " + user.name.first + " " + user.name.last}</h3>
+                <i className="fas fa-edit 2x"></i>
+              </div>
+              <p><i className="fas fa-phone-alt 3x"></i> {user.cell}</p>
+              <p><i className="fas fa-envelope 3x"></i><a href="www.gmail.com">{user.email}</a></p>
               <p>Country: {user.location.country}</p>
-              <p>City: {user.location.city}</p>
               <p>State/Province: {user.location.state}</p>
+              <p>City: {user.location.city}</p>
             </div>
-            <button onClick={closeModal}>Close</button>
           </div>
         </div>
           </div>
